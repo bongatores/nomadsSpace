3f4729f19e7997f61f7003ba4d896365ad6543c3
henrique <bongador@protonmail.com>
2022-09-12 17:41:27 -0500

    added streamr

---
+++ src/hooks/Stream.js
@@ -0,0 +1,44 @@
+import { StreamrClient } from 'streamr-client';
+
+
+
+let _streamr;
+
+
+export function createStreamRClient() {
+
+
+    const streamr = new StreamrClient({
+        auth: {
+            // who fking cares
+            ethereum: window.ethereum
+        }
+    })
+    _streamr = streamr;
+
+    console.log("client created");
+
+}
+
+
+
+
+// Subscribe to a stream
+
+
+
+export async function subscribeStreamR(id,handleMessages) {
+
+    await _streamr.subscribe({
+        stream: id,
+    },handleMessages)
+
+    console.log("streamr Subscription started");
+}
+
+
+
+export function publishMessageStreamr(id,message) {
+    // Publish messages to a stream
+    _streamr.publish(id,message);
+}
